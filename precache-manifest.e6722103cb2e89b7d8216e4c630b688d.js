2460dfd70d7460b6d0c0992e277c564c0c4a3474
ian gong <200804632@qq.com>
2019-12-15 10:27:27 +0800

    init

---
+++ precache-manifest.e6722103cb2e89b7d8216e4c630b688d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7133661d70a3e8a23b61995de06fbad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01ac0223503d9f21bc79",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8bfacc3d8619356b2342",
+    "url": "/static/js/2.508cf09d.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.508cf09d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01ac0223503d9f21bc79",
+    "url": "/static/js/main.df42ba45.chunk.js"
+  },
+  {
+    "revision": "2254e85084503d8e6430",
+    "url": "/static/js/runtime-main.ad1c004d.js"
+  }
+]);
